c951b2b3b92f2fc07e4b88c64aa4575fa928b8d2
jacob <jmatthewbrown13@gmail.com>
2024-04-08 17:52:49 -0700

    shop list

--- client/src/components/ShopList/index.jsx
+++ client/src/components/ShopList/index.jsx
@@ -1,9 +1,16 @@
-const ShopList = () => {
+const ShopList = ({ clickOnList }) => {
+
+    // const [ setSelectedShop ] = useState(null);
+
+    
 
     return (
         <>
-            <div>
-                Shop list
+            <div className="flex-column">
+                <button className="btn" onClick={() => clickOnList('Futuro')}>Futuro</button>
+                <button className="btn" onClick={() => clickOnList('Space Coffee')}>Space Coffee</button>
+                <button className="btn" onClick={() => clickOnList('Lux Central')}>Lux Central</button>
+                <button className="btn" onClick={() => clickOnList('Aftermarket')}>aftermarket</button>
             </div>
         </>
     )
