be50017a2566199829f969b30b36ad3779157f66
SamDam8 <wangombesamy@gmail.com>
2024-04-09 19:55:43 -0500

    mongobd update

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,24 +1,24 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Coment } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const thoughtSeeds = require('./commentSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
-    await cleanDB('Thought', 'thoughts');
+    await cleanDB('Comment', 'comments');
 
     await cleanDB('User', 'users');
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    for (let i = 0; i < commentSeeds.length; i++) {
+      const { _id, commentAuthor } = await Comment.create(commentSeeds[i]);
       const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
+        { username: commentAuthor },
         {
           $addToSet: {
-            thoughts: _id,
+            comments: _id,
           },
         }
       );
